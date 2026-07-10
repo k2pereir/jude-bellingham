634a694f8bbd4eebc9fb3b4f2385b1faf7a8f7af
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2026-07-10 18:07:02 -0400

    nothing was done

--- model.py
+++ model.py
@@ -6,3 +6,15 @@ import pandas as pd
 import requests
 import xgboost as xgb
 import matplotlib
+
+from datetime import date 
+
+def predict(a, b):
+    #TODO: predict
+    pass
+
+def main():
+    today = date.today()
+    fixtures = pd.read_csv("data/fixtures.csv")
+
+
